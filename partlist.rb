8f2fcb61d1dab1057b15691ebc7e7a38a68aed36
Simon Schubert <2@0x2c.org>
2017-06-12 22:46:05 +0200

    past modifications

--- partlist.rb
+++ partlist.rb
@@ -1,6 +1,60 @@
 require 'nokogiri'
+require 'optparse'
 
-x = Nokogiri::XML($stdin.read)
+def print_csv(ary, dest)
+  ary.map do |elems|
+    l = elems.map do |e|
+      if /["',\\]/ === e
+        e = '"%s"'%e.gsub(/[\\"]/, '\\\1')
+      end
+      e
+    end.join(",")
+    dest.puts l
+  end
+end
+
+def print_org(ary, dest)
+  widths = ary.reduce([0]*ary.first.length) do |memo, elems|
+    memo.zip(elems.map(&:to_s).map(&:length)).map(&:max)
+  end
+  align = ary[1..-1].reduce([true]*ary.first.length) do |memo, elems|
+    memo.zip(elems.map(&:to_s).map{|e|/^[0-9.]*$/===e}).map(&:all?)
+  end
+
+  header = true
+  ary.map do |elems|
+    l = align.zip(widths, elems).map do |a|
+      (a.first ? "%*s" : "%-*s") % a[1..-1]
+    end.join(' | ')
+    dest.puts "| #{l} |"
+    if header
+      header = false
+      dest.puts '+%s+' % widths.map{|w| '-'*(w+2)}.join('+')
+    end
+  end
+end
+
+outformat = :csv
+if $stdout.isatty
+  outformat = :org
+end
+
+OptionParser.new do |opts|
+  opts.on('--fmt FORMAT') do |fmt|
+    outformat = fmt.to_sym
+  end
+end.parse!
+
+case ARGV.length
+when 0
+  input = $stdin
+when 1
+  input = File.open(ARGV[0])
+else
+  raise "invalid argument count"
+end
+
+x = Nokogiri::XML(input)
 
 parts = x.xpath('//export/components/comp').map do |n|
   {
@@ -15,12 +69,18 @@ end
 
 parts.select!{|p| !['np', 'nopart'].include?p[:place].downcase }
 
-grouped = parts.group_by {|n| [n[:footprint], n[:bom], n[:value], n[:place]]}.values
+grouped = parts.group_by {|n|
+  if n[:bom] && n[:bom] != ""
+    n[:bom]
+  else
+    [n[:bom], n[:footprint], n[:value], n[:place]]
+  end
+}.values
 
 res = grouped.map do |group|
   namedpart = group.first.dup
   refs = group.map{|n| n[:ref]}
-  namedpart[:ref] = refs.join(" ")
+  namedpart[:ref] = refs.sort_by{|s| s.scan(/[\d]+|[^\d]+/).map{|e| /\d+/ === e ? e.to_i : e}}.join(" ")
   namedpart[:count] = refs.count
   namedpart
 end
@@ -28,6 +88,4 @@ end
 restab = [res.first.keys]
 restab += res.map{|n| n.values}
 
-resstr = restab.map{|line| line.join("\t")}.join("\n")
-
-$stdout.puts resstr
+send("print_#{outformat}", restab, $stdout)
